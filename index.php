04d856ba39df87e618132892d871570d638559c3
Wendy Dwumfuor <maadwumfuor2005@gmail.com>
2026-09-11 18:52:01 +0100

    edited the index file

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+require "db.php";
+
+$result=$conn->query("SELECT * FROM expenses ORDER BY created_at DESC ");
+
+?>
+
+<!Doctype html>
+<html>
+<head lang="en">
+<Title>My Expense App</Title>
+</head>
+<body>
+
+<h1>My Expenses</h1>
+<a href="create.php">+ Add Expense</a>
+<?php while ($row = $result->fetch_assoc()): ?>
+<div>
+<h3><?php echo htmlspecialchars($row['title']); ?></h3>
+<p><?php echo htmlspecialchars($row['description']); ?></p>
+<span><?php echo $row['status']; ?></span>
+<a href="edit.php?id=<?php echo $row['id']; ?>">Edit</a>
+<a href="delete.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Delete
+this task?');">Delete</a>
+</div>
+<?php endwhile; ?>
+
+</body>
+</html>
+
+<?php $conn->close();?>
