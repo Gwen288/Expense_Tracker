2b565b2ffc5ef408c24c2de39cdfd6b657b1d00a
Wendy Dwumfuor <maadwumfuor2005@gmail.com>
2026-09-11 19:20:15 +0100

    edited the setup file

--- setup.php
+++ setup.php
@@ -1,6 +1,15 @@
 <?php
 
-require "db.php";
+$host = "localhost";
+$db_user = "root";
+$db_pass = "";
+
+$conn = new mysqli($host, $db_user, $db_pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 
 //creating the database
 
